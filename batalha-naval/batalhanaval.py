666ee9dae2fbf02584465823bcfa1580f737d7a8
Guilherme <guilhermecosta1706@gmail.com>
2026-08-28 14:41:25 -0300

    alteraçoes

--- batalha-naval/batalhanaval.py
+++ batalha-naval/batalhanaval.py
@@ -20,12 +20,15 @@ while acertou==False:
     linha_tiro= int(input("Escolha a linha de 0 a 4: "))
     coluna_tiro= int(input("Escolha a coluna de 0 a 4: "))
 
-    if linha_tiro== navio_linha and coluna_tiro== navio_coluna:
+    if linha_tiro < 0 or linha_tiro > 4 or coluna_tiro < 0 or coluna_tiro > 4:
+        print("Digite um numero dentro da linha e da coluna!")
+
+    elif linha_tiro== navio_linha and coluna_tiro== navio_coluna:
         print("ACERTOU O NAVIO!")
         tabuleiro[linha_tiro][coluna_tiro] = "X"
         acertou=True
+        mostrar_tabuleiro()
     else:
         print("ACERTOU NA ÁGUA!")
         tabuleiro[linha_tiro][coluna_tiro]= "O"
-
-    mostrar_tabuleiro()
+        mostrar_tabuleiro()
